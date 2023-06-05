6b77ec6f0a27be8e8d02b59f3de329bb7dcae1f5
Erwin <erwin.paralesda@gmail.com>
2023-06-05 12:20:47 -0500

    Add files via upload

---
+++ SyntaxOfesautoSecurityBackend/Syntax.Ofesauto.Security.Services.Api/Helpers/ApplyVersionsSwagger.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace Syntax.Ofesauto.Security.Services.Api.Helpers
+{
+
+    public class ApplyVersionsSwagger : IOperationFilter
+    {
+        public void Apply(OpenApiOperation operation, OperationFilterContext operationFilterContext)
+        {
+            if (operation.Parameters.Count > 0)
+            {
+                //var versionParameter = operation.Parameters.Single(p => p.Name == "version");
+                //operation.Parameters.Remove(versionParameter);
+            }
+        }
+    }
+
+
+    public class ReplaceVersionWithExactValueInPath : IDocumentFilter
+    {
+        public void Apply(OpenApiDocument swaggerDoc, DocumentFilterContext documentFilterContext)
+        {
+            var paths = swaggerDoc.Paths;
+            swaggerDoc.Paths = new OpenApiPaths();
+
+            foreach (var path in paths)
+            {
+                var key = path.Key.Replace("v{version}", swaggerDoc.Info.Version);
+                var value = path.Value;
+                swaggerDoc.Paths.Add(key, value);
+            }
+        }
+    }
+}
+
