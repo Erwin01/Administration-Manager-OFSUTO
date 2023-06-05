d8b66a3b430fd3c0f3c025991aae9f1ded1416d3
Erwin <erwin.paralesda@gmail.com>
2023-06-05 15:56:48 -0500

    Add files via upload

---
+++ SyntaxOfesautoSecurityBackend/Syntax.Ofesauto.Security.Transversal.Logging/LoggerAdapter.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Extensions.Logging;
+using Syntax.Ofesauto.Security.Transversal.Common;
+
+namespace Syntax.Ofesauto.Security.Transversal.Logging
+{
+    public class LoggerAdapter<T> : IAppLogger<T>
+    {
+
+
+        /// <summary>
+        /// Globals variable
+        /// </summary>
+        /// 
+        private readonly ILogger<T> _logger;
+
+
+        #region [ ILOGGER FACTORY ]
+        /// <summary>
+        /// Exceptions traceability method
+        /// </summary>
+        /// <param name="loggerFactory"></param>
+        public LoggerAdapter(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<T>();
+        }
+
+        public void LogInformation(string message, params object[] args)
+        {
+            _logger.LogInformation(message, args);
+        }
+        public void LogWarning(string message, params object[] args)
+        {
+            _logger.LogWarning(message, args);
+        }
+
+        public void LogError(string message, params object[] args)
+        {
+            _logger.LogError(message, args);
+        }
+        #endregion
+        
+
+    }
+}
