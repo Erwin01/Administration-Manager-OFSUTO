11740ab669a77e1cb6dbdadf8939594a6ff598fb
Erwin <erwin.paralesda@gmail.com>
2023-06-05 10:55:13 -0500

    Add files via upload

---
+++ SyntaxOfesautoSecurityBackend/Syntax.Ofesauto.Infraestructure.Data/ConnectionFactory.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Extensions.Configuration;
+using Syntax.Ofesauto.Security.Transversal.Common;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Syntax.Ofesauto.Security.Infraestructure.Data
+{
+    public class ConnectionFactory : IConnectionFactory
+    {
+        /// <summary>
+        /// Object allows accessing the properties of different projects
+        /// </summary>
+        private readonly IConfiguration _configuration;
+
+        /// <summary>
+        /// Method to connect to the database and return the instance of the connection
+        /// </summary>
+        /// <param name="configuration"></param>
+        /// 
+        #region [ ICONFIGURATION ]
+        public ConnectionFactory(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        #endregion
+
+        /// <summary>
+        /// Method to access database and returns open connection instance
+        /// </summary>
+        /// 
+        #region [ GET CONNECTION ]
+        public IDbConnection GetConnection
+        {
+            get
+            {
+                var sqlConnection = new SqlConnection();
+
+                if (sqlConnection == null)
+                {
+                    return null;
+                }
+                else
+                {
+                    sqlConnection.ConnectionString = _configuration.GetConnectionString("Ofesauto");
+                    sqlConnection.Open();
+
+                    return sqlConnection;
+                }
+            }
+        }
+        #endregion
+    }
+}
---
+++ SyntaxOfesautoSecurityBackend/Syntax.Ofesauto.Infraestructure.Data/CustomDataContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Syntax.Ofesauto.Security.Domain.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Syntax.Ofesauto.Security.Infraestructure.Data
+{
+    public class CustomDataContext : DbContext
+    {
+        public CustomDataContext(DbContextOptions options) : base(options)
+        {
+        }
+        public virtual DbSet<User> User { get; set; }
+        public virtual DbSet<UserType> UserType { get; set; }
+    }
+}
