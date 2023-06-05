043b0a1db3dc3984be30dc49afc7755061c05f58
Erwin <erwin.paralesda@gmail.com>
2023-06-05 12:20:07 -0500

    Add files via upload

---
+++ SyntaxOfesautoSecurityBackend/Syntax.Ofesauto.Security.Services.Api/Email/IEmailSender.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+
+namespace Syntax.Ofesauto.Security.Services.Api.Email
+{
+    public interface IEmailSender
+    {
+        void SendEmail(Message message);
+
+        Task SendEmailAsync(Message message);
+    }
+}
